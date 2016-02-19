6a91fb9ecd25a8a1c46247781c92ebde40f81525
vsuarez <vsuarez@inerza.com>
2016-02-19 03:40:07 +0000

    Added GPL v3 license header.

--- src/main/java/vsuarez/isradeployer/Cli.java
+++ src/main/java/vsuarez/isradeployer/Cli.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Víctor Suárez <victorjss@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package vsuarez.isradeployer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,7 +37,7 @@ import java.util.stream.IntStream;
 
 /**
  *
- * @author vsuarez
+ * @author  Víctor Suárez <victorjss@gmail.com>
  */
 public class Cli {
     static final List<String> VALID_PARS =  Arrays.asList(new String[] {
